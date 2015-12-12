2899a98598be9b6782048df2808401a3c7c0fb53
Siim Kallari <skallari@klass2-23-l.intra.itcollege.ee>
2015-12-12 14:30:16 +0200

    Added buttons and some functionality

--- src/PlanningWindow.java
+++ src/PlanningWindow.java
@@ -1,7 +1,9 @@
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -11,19 +13,46 @@ import javafx.stage.Stage;
  */
 public class PlanningWindow {
 
-    private HBox buttonPlacement;
-    private VBox textPlacement;
+    private HBox mainBtnLoc;
+    private HBox semBtnLoc;
+    private VBox textLoc;
     private Text intro;
     private Stage stage;
+    private Button button;
+    private boolean clicked = true;
+    private String activate = "-fx-base: #000000";
+    private String deactivate = "-fx-base: #ffffff";
 
     public PlanningWindow() {
 
         stage = new Stage();
-        textPlacement = new VBox();
-        buttonPlacement = new HBox();
-        Scene scene = new Scene(textPlacement, 500, 600);
+
+        textLoc = new VBox();
+        mainBtnLoc = new HBox();
+
+
+        Scene scene = new Scene(textLoc, 500, 600);
+
+        // Main buttons to choose year/summary
+
+        Button firstYear = this.getButton("1");
+        this.setStyle(firstYear);
+        Button secondYear = this.getButton("2");
+        this.setStyle(secondYear);
+        Button thirdYear = this.getButton("3");
+        this.setStyle(thirdYear);
+        Button fourthYear = this.getButton("4");
+        this.setStyle(fourthYear);
+        Button summary = this.getButton("Kokkuvõte");
+        this.setStyle(summary);
+
+        mainBtnLoc.getChildren().addAll(firstYear, secondYear, thirdYear, fourthYear, summary);
+        mainBtnLoc.setPadding(new Insets(10));
+
         intro = this.getIntro();
-        textPlacement.getChildren().addAll(intro, buttonPlacement);
+
+        textLoc.getChildren().addAll(intro, mainBtnLoc);
+        textLoc.setPadding(new Insets(10));
 
         stage.setScene(scene);
         stage.show();
@@ -36,6 +65,42 @@ public class PlanningWindow {
         intro.setFont(new Font(15));
 
         return intro;
+
+    }
+
+    private Button getButton(String name) {
+
+        button = new Button(name);
+        button.setPrefSize(110, 20);
+
+        return button;
+    }
+
+    private void setStyle(Button button) {
+        button.setOnAction(event -> {
+            toggleSelected(button);
+            showExtraBtns();
+        });
+    }
+
+    private void toggleSelected(Button button) {
+        if (button.getStyle().equals(deactivate)) {
+            button.setStyle(activate);
+        } else {
+            button.setStyle(deactivate);
+        }
+    }
+
+    private void showExtraBtns() {
+
+        semBtnLoc = new HBox();
+
+        // Semester specific buttons.
+
+        Button autumn = this.getButton("Sügis");
+        Button spring = this.getButton("Kevad");
+
+        semBtnLoc.getChildren().addAll(autumn, spring);
 
     }
 }
